6eb4281c5a33dce0015a53184b6ddb6d6e714159
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2023-04-26 15:20:37 +0100

    feat: adicionando a pasta services para conexao com a api

---
+++ src/services/index.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+import nookies from "nookies"
+
+
+export const api = axios.create(
+    {
+        // baseURL: "https://portal-das-escolas-api.onrender.com",
+        baseURL: "http://localhost:4444",
+        Headers: {
+            "content-Type": "application/json",
+            Authorization: `Bearer ${nookies.get(null).token}`,
+        }
+    }
+)
